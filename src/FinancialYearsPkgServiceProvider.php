911595c1d70b32368f25264871ea2ef5916e51d9
Abdul <abdul@uitoux.in>
2020-01-27 11:44:04 +0530

    u

--- src/FinancialYearsPkgServiceProvider.php
+++ src/FinancialYearsPkgServiceProvider.php
@@ -17,6 +17,8 @@ class FinancialYearsPkgServiceProvider extends ServiceProvider {
 		$this->loadViewsFrom(__DIR__ . '/views', 'financial-years-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
+			__DIR__ . '/database/seeds/client' => 'database/seeds',
+			__DIR__ . '/config/config.php' => config_path('financial-years-pkg.php'),
 		]);
 	}
 
