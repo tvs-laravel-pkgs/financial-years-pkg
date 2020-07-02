87cd50dc4e702f537cfb38ba46d8ee339c010d94
arun-uitoux <arunkumar@uitoux.in>
2020-07-02 20:47:07 +0530

    finance yr model import chagnes

--- src/models/FinancialYear.php
+++ src/models/FinancialYear.php
@@ -2,18 +2,88 @@
 
 namespace Abs\FinancialYearsPkg;
 
-use Auth;
-use Illuminate\Database\Eloquent\Model;
+use Abs\HelperPkg\Traits\SeederTrait;
+use App\BaseModel;
+// use Illuminate\Database\Eloquent\Model;
+use App\Company;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class FinancialYear extends Model {
+class FinancialYear extends BaseModel {
 	use SoftDeletes;
+	use SeederTrait;
 	protected $table = 'financial_years';
 	protected $fillable = [
 		'code',
 		'from',
 		'company_id',
 	];
+
+	protected static $excelColumnRules = [
+		'Code' => [
+			'table_column_name' => 'code',
+			'rules' => [
+				'required' => [
+				],
+			],
+		],
+		'From' => [
+			'table_column_name' => 'from',
+			'rules' => [
+				'required' => [
+				],
+			],
+		],
+	];
+
+	public static function saveFromObject($record_data) {
+		$record = [
+			'Company Code' => $record_data->company_code,
+			'Code' => $record_data->code,
+			'From' => $record_data->from,
+		];
+		return static::saveFromExcelArray($record);
+	}
+
+	public static function saveFromExcelArray($record_data) {
+		$errors = [];
+		$company = Company::where('code', $record_data['Company Code'])->first();
+		if (!$company) {
+			return [
+				'success' => false,
+				'errors' => ['Invalid Company : ' . $record_data['Company Code']],
+			];
+		}
+
+		if (!isset($record_data['created_by_id'])) {
+			$admin = $company->admin();
+
+			if (!$admin) {
+				return [
+					'success' => false,
+					'errors' => ['Default Admin user not found'],
+				];
+			}
+			$created_by_id = $admin->id;
+		} else {
+			$created_by_id = $record_data['created_by_id'];
+		}
+
+		$record = self::firstOrNew([
+			'company_id' => $company->id,
+			'from' => $record_data['From'],
+		]);
+
+		$result = Self::validateAndFillExcelColumns($record_data, Static::$excelColumnRules, $record);
+		if (!$result['success']) {
+			return $result;
+		}
+		$record->company_id = $company->id;
+		$record->created_by_id = $created_by_id;
+		$record->save();
+		return [
+			'success' => true,
+		];
+	}
 
 	// public static function createFromObject($record_data) {
 
